857dcd5d97010c0b156864636401fec5df2fa826
Roman Didukh <newrdlink@yandex.ru>
2020-06-24 20:05:27 +0300

    24-06-2020

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-function createParagraph() {
-  let para = document.createElement("p");
-  para.textContent = "You clicked the button!";
-  document.body.appendChild(para);
-  para.setAttribute(color, "white");
-}
+const date = new Date();
+let month = date.getUTCMonth();
 
-const buttons = document.querySelectorAll("button");
+console.log(date);
+console.log(month + 1);
 
-for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", createParagraph);
-}
+// function createParagraph() {
+//   let para = document.createElement("p");
+//   para.textContent = "You clicked the button!";
+//   document.body.appendChild(para);
+//   para.setAttribute(color, "white");
+// }
+
+// const buttons = document.querySelectorAll("button");
+
+// for (let i = 0; i < buttons.length; i++) {
+//   buttons[i].addEventListener("click", createParagraph);
+// }
